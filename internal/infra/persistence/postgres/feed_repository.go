757989c107f5fa4bf42f3ad0b2915b80a41a2e22
55408suga <55408suga.ka@gmail.com>
2026-03-18 22:21:32 +0900

    feat: implement feed repository

---
+++ internal/infra/persistence/postgres/feed_repository.go
@@ -0,0 +1,116 @@
+package postgres
+
+import (
+	"context"
+
+	"rss_reader/internal/domain/model"
+	"rss_reader/internal/domain/repository"
+	"rss_reader/internal/infra/persistence/postgres/generated"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type FeedRepository struct {
+	db      *pgxpool.Pool
+	queries *generated.Queries
+}
+
+func NewFeedRepository(db *pgxpool.Pool) repository.FeedRepository {
+	return &FeedRepository{
+		db:      db,
+		queries: generated.New(db),
+	}
+}
+
+func (r *FeedRepository) SaveFeed(ctx context.Context, feed *model.Feed) error {
+	params := generated.SaveFeedParams{
+		ID:          feed.ID,
+		Title:       feed.Title,
+		UpdatedAt:   feed.UpdatedAt,
+		FeedUrl:     feed.FeedURL,
+		WebsiteUrl:  feed.WebsiteURL,
+		Description: pgtype.Text{String: feed.Description, Valid: feed.Description != ""},
+		Language:    pgtype.Text{String: feed.Language, Valid: feed.Language != ""},
+	}
+	return r.queries.SaveFeed(ctx, params)
+}
+
+func (r *FeedRepository) GetFeed(ctx context.Context, feedID uuid.UUID) (*model.Feed, error) {
+	feed, err := r.queries.GetFeedByID(ctx, feedID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Feed{
+		ID:          feed.ID,
+		Title:       feed.Title,
+		UpdatedAt:   feed.UpdatedAt,
+		FeedURL:     feed.FeedUrl,
+		WebsiteURL:  feed.WebsiteUrl,
+		Description: feed.Description.String,
+		Language:    feed.Language.String,
+	}, nil
+}
+
+func (r *FeedRepository) GetAllFeeds(ctx context.Context) ([]*model.Feed, error) {
+	feeds, err := r.queries.GetAllFeeds(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	//convert generated.Feed to model.Feed
+	feedModels := make([]*model.Feed, len(feeds))
+	for i, feed := range feeds {
+		feedModels[i] = &model.Feed{
+			ID:          feed.ID,
+			Title:       feed.Title,
+			UpdatedAt:   feed.UpdatedAt,
+			FeedURL:     feed.FeedUrl,
+			WebsiteURL:  feed.WebsiteUrl,
+			Description: feed.Description.String,
+			Language:    feed.Language.String,
+		}
+	}
+	return feedModels, nil
+}
+
+func (r *FeedRepository) GetArticles(ctx context.Context, feedID uuid.UUID) ([]*model.Article, error) {
+	articles, err := r.queries.GetArticles(ctx, feedID)
+	if err != nil {
+		return nil, err
+	}
+
+	//convert generated.Article to model.Article
+	articleModels := make([]*model.Article, len(articles))
+	for i, article := range articles {
+		articleModels[i] = &model.Article{
+			ID:          article.ID,
+			Title:       article.Title,
+			Description: article.Description.String,
+			PublishedAt: article.PublishedAt,
+			WebsiteURL:  article.WebsiteUrl,
+			Content:     article.Content.String,
+			FeedID:      article.FeedID,
+		}
+	}
+	return articleModels, nil
+}
+
+func (r *FeedRepository) UpdateFeed(ctx context.Context, feed *model.Feed) error {
+	params := generated.UpdateFeedParams{
+		ID:          feed.ID,
+		Title:       feed.Title,
+		UpdatedAt:   feed.UpdatedAt,
+		FeedUrl:     feed.FeedURL,
+		WebsiteUrl:  feed.WebsiteURL,
+		Description: pgtype.Text{String: feed.Description, Valid: feed.Description != ""},
+		Language:    pgtype.Text{String: feed.Language, Valid: feed.Language != ""},
+	}
+	return r.queries.UpdateFeed(ctx, params)
+}
+
+func (r *FeedRepository) DeleteFeed(ctx context.Context, feedID uuid.UUID) error {
+	return r.queries.DeleteFeed(ctx, feedID)
+}
